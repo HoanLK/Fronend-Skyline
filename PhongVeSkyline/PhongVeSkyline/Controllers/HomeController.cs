84a3ba7acec3615e868d2b3f004297db80b4418f
lathanh1108 <lathanh1108@gmail.com>
2016-11-25 10:54:08 +0700

    Init
    full trang phòng vé skyline

---
+++ PhongVeSkyline/PhongVeSkyline/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhongVeSkyline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Venoidia()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Vequocte()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Tintuc()
+        {
+            ViewBag.Message = "Your news page.";
+
+            return View();
+        }
+
+        public ActionResult Gioithieu()
+        {
+            ViewBag.Message = "Your about page.";
+
+            return View();
+        }
+
+        public ActionResult Lienhe()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
